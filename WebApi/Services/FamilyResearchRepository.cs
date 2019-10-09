65d6d6883a233db49418edad627eac5bb5054dce
brandochn <brandochn@gmail.com>
2019-10-08 23:07:46 -0500

    Added FamilyResearchRepository class

---
+++ WebApi/Services/FamilyResearchRepository.cs
@@ -0,0 +1,13 @@
+using Naandi.Shared.Models;
+using Naandi.Shared.Services;
+
+namespace WebApi.Services
+{
+    public class FamilyResearchRepository : IFamilyResearch
+    {
+        public void Add(FamilyResearch familyResearch)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
