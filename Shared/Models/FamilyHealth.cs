0d30a602912c63199b8f3e33e110f1cd478909e8
gdlhicha <gdlhicha@GDLM09464>
2019-11-08 16:41:51 -0600

    Add  Salud familiar section in showform view

--- Shared/Models/FamilyHealth.cs
+++ Shared/Models/FamilyHealth.cs
@@ -3,13 +3,13 @@ namespace Naandi.Shared.Models
     public class FamilyHealth
     {
         public int Id { get; set; }
-        public string Familyhealthstatus { get; set; }
-        public string Derechohambienteaserviciosdesalud { get; set; }
+        public string FamilyHealthStatus { get; set; }
+        public string DerechoHambienteAServiciosDeSalud { get; set; }
         public string Tipo { get; set; }
-        public string Enfermedadescronicasdegenerativas { get; set; }
-        public string Consumodetabaco { get; set; }
-        public string Consumodealcohol { get; set; }
-        public string Consumodedrogas { get; set; }
+        public string EnfermedadesCronicasDegenerativas { get; set; }
+        public string ConsumoDeTabaco { get; set; }
+        public string ConsumoDeAlcohol { get; set; }
+        public string ConsumoDeDrogas { get; set; }
         public string Comments { get; set; }
     }
 }
